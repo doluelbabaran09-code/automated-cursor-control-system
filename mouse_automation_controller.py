af4e92db4463a1b4ae4dea87f656e959285f8007
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-03-03 15:26:47 +0800

    Update mouse_automation_controller.py

---
+++ mouse_automation_controller.py
@@ -0,0 +1,28 @@
+import time
+import threading
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener, KeyCode
+
+toggole_key = KeyCode(char="t")
+
+clicking = False
+mouse = Controller()
+
+
+
+def clicker():
+    while True:
+        if clicking:
+            mouse.click(Button.left, 1)
+            time.sleep(0.001)
+        
+def toggle_event(key): 
+    if key == toggole_key:
+        global clicking
+        clicking = not clicking
+
+click_thread = threading.Thread(target=clicker)
+click_thread.start()
+
+with Listener(on_press=toggle_event) as listener:
+    listener.join()
